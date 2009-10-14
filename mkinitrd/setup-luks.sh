d34f22403f2774373fd2d7829d3d3f0250613666
Ludwig Nussel <ludwig.nussel@suse.de>
2009-10-14 10:47:23 +0200

    fix autodetetection of root on LVM on LUKS (bnc#528474)

--- mkinitrd/setup-luks.sh
+++ mkinitrd/setup-luks.sh
@@ -3,12 +3,19 @@
 #%stage: crypto
 #
 
+dbg()
+{
+    test -n "$mkinitrd_luks_debug" || return 0
+    echo "$@"
+}
+
 # search for entries that have the 'initrd' option set
 find_crypttab_initrd()
 {
     test -s /etc/crypttab || return
 
     local addit extraopts
+    local name physdev keyfile options dummy
 
     while read name physdev keyfile options dummy; do
 	case "$name" in
@@ -38,44 +45,72 @@ find_crypttab_initrd()
 	    if [ -n "$extraopts" ]; then
 		echo "/etc/crypttab: $name has extra options, not supported by the initrd"
 	    else
-		luks_add_device="$luks_add_device /dev/mapper/$name"
+		luks_add_device+=("/dev/mapper/$name")
 	    fi
 	fi
     done < /etc/crypttab
 }
 
-if [ -x /sbin/cryptsetup -a -x /sbin/dmsetup ] ; then
+isset()
+{
+    eval "test -n \"\$$1\""
+}
+
+find_luks_devices()
+{
     luks_blockdev=
-    luks_add_device="$blockdev"
+    luks_add_device=()
     find_crypttab_initrd
+    set -- "${luks_add_device[@]}" $blockdev
     # bd holds the device we see the decrypted LUKS partition as
-    for bd in $luks_add_device ; do
+    while [ "$#" -gt 0 ]; do
+	bd="$1"; shift
     	luks_name=
 	update_blockdev $bd
 	luks_blockmajor=$blockmajor
 	luks_blockminor=$blockminor
+	dbg "finding deps of $bd ($blockmajor:$blockminor) ..."
+	deps=$(dm_resolvedeps $bd)
 	# luksbd holds the device, LUKS is running on
-	for luksbd in $(dm_resolvedeps $bd); do # should only be one for luks
-		[ $? -eq 0 ] || return 1
-		update_blockdev $luksbd
-		if /sbin/cryptsetup isLuks $luksbd 2>/dev/null; then
-			root_luks=1
-			tmp_root_dm=1 # luks needs dm
-
-			luks_name="$(dmsetup -c info -o name --noheadings -j $luks_blockmajor -m $luks_blockminor)"
-			eval luks_${luks_name}=$(beautify_blockdev ${luksbd})
-			save_var luks_${luks_name}
-
-			luks="$luks $luks_name"
-			echo "enabling LUKS support for $luksbd"
-			luks_blockdev="$luks_blockdev $luksbd"
+	for luksbd in $deps; do # should be only one for luks
+	    update_blockdev $luksbd
+	    dbg -n "isLuks $luksbd ... "
+	    if ! /sbin/cryptsetup isLuks $luksbd 2>/dev/null; then
+		dbg -n "no"
+		if [ "$blockdriver" = "device-mapper" ]; then
+		    dbg -n ", but dm, requeue"
+		    # the block device is on dm itself
+		    set -- "$@" "$luksbd"
 		fi
+		dbg
+		continue
+	    fi
+	    dbg "yes"
+	    root_luks=1
+	    tmp_root_dm=1 # luks needs dm
+
+	    luks_name="$(dmsetup -c info -o name --noheadings -j $luks_blockmajor -m $luks_blockminor)"
+	    if isset "luks_${luks_name}"; then
+		dbg "$luks_name already handled"
+		continue
+	    fi
+	    eval luks_${luks_name}=$(beautify_blockdev ${luksbd}) || continue
+	    save_var luks_${luks_name}
+	    ! isset luks_${luks_name}_options || save_var luks_${luks_name}_options
+
+	    luks="$luks $luks_name"
+	    echo "enabling LUKS support for $luksbd ($luks_name)"
+	    luks_blockdev="$luks_blockdev $luksbd"
 	done
 	if [ ! "$luks_name" ]; then # no luks found
-		luks_blockdev="$luks_blockdev $bd"
+	    luks_blockdev="$luks_blockdev $bd"
 	fi
     done
     blockdev="$luks_blockdev"
+}
+
+if [ -x /sbin/cryptsetup -a -x /sbin/dmsetup ] ; then
+    find_luks_devices
 fi
 
 if [ -n "$root_luks" ]; then
